2f5ceae8f8ef60716c968ebea28deb5076a2404f
Gabriella Mondvay <gabriellamondvay@gmail.com>
2023-12-18 21:37:50 +0100

    Color palette is better. Nothing else works yet

---
+++ websiteDesign/README.md
@@ -0,0 +1,21 @@
+# Website Design
+
+## Description
+
+This was supposed to be a simple website project for Codecademy to create a directory of fonts and colors to keep as reference. I decided to challenge myself and created both a color palette generator and font pairing generator, complete with a tiny website preview.
+
+## Color Generator
+
+Works by selecting a random light hsl color and then randomizing a second, darker color that is close in hue to the first one. After that it randomizes two very muted colors, one light and one dark, meant to be used in place of black and white as these often look too strong and unnatural. The last color is entirely random and is meant to be used as an accent color.
+
+## Font Pairings
+
+Selects three different fonts:
+* A display or script font, meant to be used only for big headlines
+* A title font, easier to read than the first font but still has some impact to it
+* A paragraph font
+
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
--- websiteDesign/colorGenerator.js
+++ websiteDesign/colorGenerator.js
@@ -1,49 +1,54 @@
 const btn = document.querySelector("button");
-const rgbs = document.querySelectorAll(".rgb");
 const hsls = document.querySelectorAll(".hsl");
 
-
-// Generate the first hue. Global variable so it can be accessed inside functions
+// Generate the first hue as a global variable so it can be accessed inside functions
 let baseHue = () => Math.floor(Math.random() * 360);
 
-const generateColor = (hue, saturation, lightness) => {
+// Function to return generated values as hsl string
+let color = (generateColor = (hue, saturation, lightness) => {
   return `hsl(${hue}, ${saturation}%, ${lightness}%)`;
-};
+});
 // Generate a light muted color and assign lightHue the hue of that color
 const lightMutedColor = () => {
   let saturation = Math.floor(Math.random() * 30) + 30;
   let lightness = Math.floor(Math.random() * 20) + 60;
-  return generateColor(baseHue, saturation, lightness)
+  return generateColor(baseHue, saturation, lightness);
 };
 
 // Generate a dark muted color within 25 degrees of lightHue
 const darkMutedColor = () => {
   let hue = (baseHue + Math.floor(Math.random() * 50) - 25 + 360) % 360;
   let saturation = Math.floor(Math.random() * 30) + 30;
-  let lightness = Math.floor(Math.random() * 20) + 20;
-  return generateColor(hue, saturation, lightness)
-};
-
-const lightGrey = () => {
-  let lightness = Math.floor(Math.random() * 20) + 70;
-  return generateColor(0, 0, lightness);
-};
-
-const darkGrey = () => {
-  let lightness = Math.floor(Math.random() * 20) + 20;
-  return generateColor(0, 0, lightness);
-}
-// Generate accent color
-const accentColor = () => {
-  let hue = Math.floor(Math.random() * 360);
-  let saturation = Math.floor(Math.random() * 100);
-  let lightness = Math.floor(Math.random() * 100);
+  let lightness = Math.floor(Math.random() * 20) + 30;
   return generateColor(hue, saturation, lightness);
 };
 
+// Generate a light grey color within 25 degrees of lightHue
+const lightGrey = () => {
+  let hue = (baseHue + Math.floor(Math.random() * 50) - 25 + 360) % 360;
+  let saturation = Math.floor(Math.random() * 7);
+  let lightness = Math.floor(Math.random() * 10) + 85;
+  return generateColor(hue, saturation, lightness);
+};
 
+// Generate a dark grey color within 25 degrees of lightHue
+const darkGrey = () => {
+  let hue = (baseHue + Math.floor(Math.random() * 50) - 25 + 360) % 360;
+  let saturation = Math.floor(Math.random() * 5) + 5;
+  let lightness = Math.floor(Math.random() * 20) + 5;
+  return generateColor(hue, saturation, lightness);
+};
+// Generate accent color on the opposite side of the color wheel from lightHue by returning the absolute value of lightHue - 180
+const accentColor = () => {
+  let hue = Math.abs(baseHue - 180);
+  let saturation = Math.floor(Math.random() * 70) + 30;
+  let lightness = Math.floor(Math.random() * 60) + 20;
+  return generateColor(hue, saturation, lightness);
+};
+
+// Function to apply colors to the page
 const applyColors = () => {
-  baseHue = Math.floor(Math.random() * 360); 
+  baseHue = Math.floor(Math.random() * 360);
   document.getElementById("color1").style.backgroundColor = lightMutedColor();
   hsls[0].innerHTML = lightMutedColor();
   document.getElementById("color2").style.backgroundColor = darkMutedColor();
